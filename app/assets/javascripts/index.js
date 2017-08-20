6d2f51ff660d559578b07fb3dac3dff4b3acdd87
andreykovalchuk <a.kovalchuk91@gmail.com>
2017-08-20 23:12:25 +0300

    Index page & user profile

---
+++ app/assets/javascripts/index.js
@@ -0,0 +1,22 @@
+$( document ).ready(function(){
+    $(".button-collapse").sideNav();
+    $('ul.tabs').tabs();
+
+});
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+});
